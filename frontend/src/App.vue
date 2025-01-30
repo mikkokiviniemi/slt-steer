37a7cb56456b1388e876ba3411dd77193da255d8
mikkokiviniemi <kiviniemi.mikko@gmail.com>
2025-01-30 19:07:11 +0200

    vue+python

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Vue + FastAPI</h1>
+    <button @click="fetchData">Fetch Data</button>
+    <ul v-if="data.length">
+      <li v-for="item in data" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return { data: [] };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/api/data");
+        this.data = response.data.data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  }
+};
+</script>
