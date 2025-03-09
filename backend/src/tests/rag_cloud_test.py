8421132e496daf9078fe7176dd314032b70b7046
mikkokiviniemi <kiviniemi.mikko@gmail.com>
2025-03-09 10:19:04 +0200

    backend refactor init

---
+++ backend/src/tests/rag_cloud_test.py
@@ -0,0 +1,11 @@
+# Unit tests for rag_cloud.py
+
+import unittest
+
+class TestRagCloud(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(1,1)
+
+if __name__ == '__main__':
+    unittest.main()
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 cd backend
 python3 -m venv venv
 source venv/bin/activate
-pip install fastapi uvicorn
+pip install fastapi uvicorn google-generativeai langchain_google_genai langchain_community langchain_text_splitters pypdf chromadb google-cloud-storage langchain-chroma PyPDF2
 cd ../frontend
 npm install
 cd ..
