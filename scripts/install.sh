70c651e9a03d3998cd57143194535ecbe6f8b5fa
mikkokiviniemi <kiviniemi.mikko@gmail.com>
2025-03-11 17:47:27 +0200

    init tests

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 cd backend
 python3 -m venv venv
 source venv/bin/activate
-pip install fastapi uvicorn google-generativeai langchain_google_genai langchain_community langchain_text_splitters pypdf chromadb google-cloud-storage langchain-chroma PyPDF2 python-dotenv
+pip install pytest fastapi uvicorn google-generativeai langchain_google_genai langchain_community langchain_text_splitters pypdf chromadb google-cloud-storage langchain-chroma PyPDF2 python-dotenv
 python src/setup_env.py
 cd ../frontend
 npm install
