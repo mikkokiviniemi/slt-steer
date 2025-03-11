18933bf80fce6b3f4b5d1cbfd9220ea98c2c72e3
mikkokiviniemi <kiviniemi.mikko@gmail.com>
2025-03-11 17:50:11 +0200

    test run for windows

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,7 +2,7 @@
 cd backend
 python -m venv venv
 call venv\Scripts\activate.bat
-pip install fastapi uvicorn google-generativeai langchain_google_genai langchain_community langchain_text_splitters pypdf chromadb google-cloud-storage langchain-chroma PyPDF2 python-dotenv
+pip install pytest fastapi uvicorn google-generativeai langchain_google_genai langchain_community langchain_text_splitters pypdf chromadb google-cloud-storage langchain-chroma PyPDF2 python-dotenv
 python src\setup_env.py
 cd ../frontend
 npm install
