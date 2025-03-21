3f345e35d1055cdc3ee1d93fd86ab881465aaa57
Elsi Häkkinen <elsi.hakkinen@tuni.fi>
2025-03-21 16:04:52 +0200

    detaljeja toimivuuteen

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,7 +2,7 @@
 cd backend
 python -m venv venv
 call venv\Scripts\activate.bat
-pip install pytest fastapi uvicorn google-generativeai langchain_google_genai langchain_community langchain_text_splitters pypdf chromadb google-cloud-storage langchain-chroma PyPDF2 python-dotenv
+pip install pytest fastapi uvicorn google-generativeai langchain_google_genai langchain_community langchain_text_splitters pypdf chromadb google-cloud-storage langchain-chroma PyPDF2 python-dotenv motor pymongo
 python src\setup_env.py
 for /f "delims=" %%i in ('dir /b /s requirements.txt 2^>nul') do (
     echo Installing dependencies from %%i...
