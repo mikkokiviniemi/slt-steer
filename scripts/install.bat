49e1330fe847a4e8edc9b71bdda84c3f9863140b
Matias <matias.haapasalmi@tuni.fi>
2025-03-26 16:45:48 +0200

    merge

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,6 +2,8 @@
 cd backend
 python -m venv venv
 call venv\Scripts\activate.bat
+pip install pytest fastapi uvicorn google-generativeai langchain_google_genai langchain_community langchain_text_splitters pypdf chromadb google-cloud-storage langchain-chroma PyPDF2 python-dotenv
+python src\setup_env.py
 for /f "delims=" %%i in ('dir /b /s requirements.txt 2^>nul') do (
     echo Installing dependencies from %%i...
     pip install -r "%%i"
