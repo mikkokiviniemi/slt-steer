12fe8cb404795a54c1b0d14a851580988114b323
ybaac <danielmilner@protonmail.com>
2025-04-08 22:30:07 +0200

    feat: added get api url from env and backend/frontend examples

--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";  // FastAPI backend URL
+const API_URL = process.env.VUE_APP_REACT_APP_API_URL || "http://127.0.0.1:8000"; // FastAPI backend URL
 
 export const createUser = async (userData) => {
   try {
